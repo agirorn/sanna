18f2542eebb2804b9d27a5abfd31c0a8d086ed53
Ægir Örn Símonarson <agirorn@gmail.com>
2022-08-01 14:36:19 +0000

    Fixed typescript type defenition

--- index.d.ts
+++ index.d.ts
@@ -4,9 +4,9 @@ interface Hook { (context: any): any; }
 interface AsyncHook { (context: any): Promise<any>; }
 interface TestSuite {
   (title: string, callback: Test|AsyncTest): any;
-  setup: (callback: Hook|AsyncHook): Promise<any>;
-  teardown: (callback: Hook|AsyncHook): Promise<any>;
-  before: (callback: Hook|AsyncHook): Promise<any>;
-  after: (callback: Hook|AsyncHook): Promise<any>;
+  setup: (callback: Hook|AsyncHook) => Promise<any>;
+  teardown: (callback: Hook|AsyncHook) => Promise<any>;
+  before: (callback: Hook|AsyncHook) => Promise<any>;
+  after: (callback: Hook|AsyncHook) => Promise<any>;
 }
 export function suite(title: string): TestSuite;
